a4ca9ce7f499cae3ffdde84e8cd684ab2df7af4a
さくちゃん <108169585+AisyA72@users.noreply.github.com>
2024-07-09 11:19:31 +0900

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <nav>
+    <a href="reservation.php">新規予約</a>
+    <a href="reservation_list.php">予約一覧</a>
+    <a href="chat.php">チャット</a>
+  </nav>
+</body>
+</html>
---
+++ public/reservation_list.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>予約一覧</title>
+</head>
+<body>
+  予約一覧
+</body>
+</html>
