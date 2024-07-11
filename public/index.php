f667d5c588467b37b7a1f60a54b7ff5e63243116
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2024-07-11 17:19:19 +0900

    add: top page header

--- public/index.php
+++ public/index.php
@@ -4,16 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-  <nav>
-    <a href="reservation.php">新規予約</a>
-    <a href="reservation_list.php">予約一覧</a>
-    <a href="chat.php">チャット</a>
-  </nav>
 
-  <div>
+  <header class="header">
+    <div class="logo">AA</div>
+    <div class="nav">
+      <input id="drawer_input" class="drawer_hidden" type="checkbox">
+      <label for="drawer_input" class="drawer_open"><span></span></label>
+      <nav class="nav_content">
+        <ul class="nav_list">
+          <li class="nav_item"><a href="reservation.php">新規予約</a></li>
+          <li class="nav_item"><a href="reservation_list.php">予約一覧</a></li>
+          <li class="nav_item"><a href="chat.php">チャット</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <main>
     トップページ
-  </div>
+</main>
 </body>
 </html>
